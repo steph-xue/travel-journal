e7dd7e3183efa6755997d31e86c03b508eb3308b
Stephanie Xue <stephaniexue3@gmail.com>
2024-07-30 15:40:26 -0700

    add photos of travel log

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ function App() {
     );
   });
 
+  cardElements.reverse();
+
   return (
     <div>
       <Navbar />
--- src/data.jsx
+++ src/data.jsx
@@ -1,32 +1,122 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Seattle Space Needle",
+        location: "Seattle, USA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        image: "fuji.png"
+        image: "seattle-space-needle.jpg"
     },
     {
         id: 2,
-        title: "Sydney Opera House",
-        location: "Australia",
+        title: "Seattle Pike Place Market",
+        location: "Seattle, USA",
         googleMapsUrl: "https://maps.app.goo.gl/JjjxC32FL35e9New6",
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        image: "sydney-opera.png"
+        image: "seattle-pike-place.jpg"
     },
     {
         id: 3,
-        title: "Geirangerfjord",
-        location: "Norway",
+        title: "Nathan Phillips Square",
+        location: "Toronto, Canada",
         googleMapsUrl: "https://maps.app.goo.gl/GjJyigB5aD6P8ZM26",
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        image: "geirangerfjord.png"
+        image: "toronto.jpg"
+    },
+    {
+        id: 4,
+        title: "Lake Louise",
+        location: "Lake Louise, Canada",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "lake-louise.jpg"
+    },
+    {
+        id: 5,
+        title: "Nakano Broadway",
+        location: "Tokyo, Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "nakano.jpg"
+    },
+    {
+        id: 6,
+        title: "Shinjuku Gyoen National Garden",
+        location: "Tokyo, Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "shinjuku.jpg"
+    },
+    {
+        id: 7,
+        title: "Osaka Castle",
+        location: "Osaka, Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "osaka-castle.jpg"
+    },
+    {
+        id: 8,
+        title: "Arashiyama Bamboo Grove",
+        location: "Kyoto, Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "arashiyama.jpg"
+    },
+    {
+        id: 9,
+        title: "Fushima Inari Taisha Shrine",
+        location: "Kyoto, Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "fushimi-inari.jpg"
+    },
+    {
+        id: 10,
+        title: "Cabo San Lucas Sandos Finisterra",
+        location: "Cabo San Lucas, Mexico",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "cabo.jpg"
+    },
+    {
+        id: 11,
+        title: "Cabo San Lucas Arch",
+        location: "Cabo San Lucas, Mexico",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "cabo-arch.jpg"
+    },
+    {
+        id: 12,
+        title: "Cabo San Jose",
+        location: "Cabo San Jose, Mexico",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        image: "cabo-san-jose.jpg"
     }
 ]
