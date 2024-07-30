f2b84f18d2f2244c5de2364f92db53e66bec872a
Stephanie Xue <stephaniexue3@gmail.com>
2024-07-30 02:25:00 -0700

    add card component for travel experiences

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Card() {
+function Card(prop) {
     return (
         <div>
-            Cards
+            <img src={`/images/${prop.image}`} alt={prop.location} />
+            <div className="location">
+                <img src="/images/pin.png" alt="pin" />
+                <p>{prop.location}</p>
+                <a href={prop.googleMapsUrl}>View on Google Maps</a>
+            </div>
+            <div className="dates">
+                <p>{prop.startDate} - {prop.endDate}</p>
+            </div>
+            <p>{prop.description}</p>
+            <hr />
         </div>
     );
 }
