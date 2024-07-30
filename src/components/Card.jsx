4c591c69c406b280cac623fda2d334a5cf461552
Stephanie Xue <stephaniexue3@gmail.com>
2024-07-30 02:57:30 -0700

    style card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 
 function Card(prop) {
     return (
-        <div>
-            <img src={`/images/${prop.image}`} alt={prop.location} />
-            <div className="location">
-                <img src="/images/pin.png" alt="pin" />
-                <p>{prop.location}</p>
-                <a href={prop.googleMapsUrl}>View on Google Maps</a>
+        <div className="card">
+            <div className="card-inner">
+                <div className="card-left">
+                    <img className="card-image" src={`/images/${prop.image}`} alt={prop.location} />
+                </div>
+                <div className="card-right">
+                    <div className="card-location">
+                        <img className="pin" src="/images/pin.png" alt="pin" />
+                        <p className="location">{prop.location}</p>
+                        <a className="location-link" href={prop.googleMapsUrl}>View on Google Maps</a>
+                    </div>
+                    <h1 className="card-title">{prop.title}</h1>
+                    <div className="card-dates">
+                        <p>{prop.startDate} - {prop.endDate}</p>
+                    </div>
+                    <p className="card-description">{prop.description}</p>
+                </div>
             </div>
-            <div className="dates">
-                <p>{prop.startDate} - {prop.endDate}</p>
-            </div>
-            <p>{prop.description}</p>
-            <hr />
+            <hr className="line" />
         </div>
     );
 }
